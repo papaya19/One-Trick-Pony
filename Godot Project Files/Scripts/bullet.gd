a4d404cc122af694c87ea55bc78beabc215d77d8
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-02-14 12:34:10 +1300

    No more bullet piercingf
    does anyone even read these

--- Godot Project Files/Scripts/bullet.gd
+++ Godot Project Files/Scripts/bullet.gd
@@ -9,3 +9,9 @@ func _process(delta: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+
+func _on_area_entered(body) -> void:
+	if body.is_in_group("enemy"):
+		queue_free()
