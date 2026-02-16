35616d71dde65586d3f11465eb5353e7ac7e1489
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-02-17 09:40:53 +1300

    folder for Isaac

--- Godot Project Files/Scripts/start.gd
+++ Godot Project Files/Scripts/start.gd
@@ -4,13 +4,16 @@ extends Control
 func _on_shotgun_pressed() -> void:
 	get_node("/root/Global/GUI/HUD").process_mode = Node.PROCESS_MODE_ALWAYS
 	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+	print("shotyy")
 
 
 func _on_assult_riffle_pressed() -> void:
 	get_node("/root/Global/GUI/HUD").process_mode = Node.PROCESS_MODE_ALWAYS
 	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+	print("arrrrr")
 
 
 func _on_sniper_pressed() -> void:
 	get_node("/root/Global/GUI/HUD").process_mode = Node.PROCESS_MODE_ALWAYS
 	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+	print("360 no scope")
