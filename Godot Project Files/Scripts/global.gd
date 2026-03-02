1ecb6ddb48dc30240325a1635e878e992a869a1f
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-03-03 10:03:50 +1300

    Sorted some stuff

--- Godot Project Files/Scripts/global.gd
+++ Godot Project Files/Scripts/global.gd
@@ -1,14 +1,14 @@
 extends Node
 
 var ship_stats = {
-	"Shotgun": {"texture_file_path": "res://Assets/Shotgun.png", 
-	"magazine_size": 6, "bullet_damage": 30, "reload_time": 1.2, "shooting_speed": 1.2, "spread": 0.3, "bullets_per_shot": 5},
+	"Shotgun": {"texture_file_path": "res://Assets/Ships/Shotgun.png", 
+	"magazine_size": 6, "bullet_damage": 30, "reload_time": 2.2, "shooting_speed": 1.2, "spread": 0.3, "bullets_per_shot": 5},
 	
-	"Assault Rifle": {"texture_file_path": "res://Assets/Assault Rifle.png", 
-	"magazine_size": 20, "bullet_damage": 60, "reload_time": 1.2, "shooting_speed": 0.3, "spread": 0, "bullets_per_shot": 1},
+	"Assault Rifle": {"texture_file_path": "res://Assets/Ships/Assault Rifle.png", 
+	"magazine_size": 20, "bullet_damage": 60, "reload_time": 1.3, "shooting_speed": 0.3, "spread": 0, "bullets_per_shot": 1},
 	
-	"Sniper": {"texture_file_path": "res://Assets/Sniper.png", 
-	"magazine_size": 3, "bullet_damage": 180, "reload_time": 1.2, "shooting_speed": 2, "spread": 0, "bullets_per_shot": 1}
+	"Sniper": {"texture_file_path": "res://Assets/Ships/Sniper.png", 
+	"magazine_size": 3, "bullet_damage": 180, "reload_time": 3.25, "shooting_speed": 1.25, "spread": 0, "bullets_per_shot": 1}
 	
 }
 
@@ -19,15 +19,13 @@ signal player_gain_energy(energy_value: int)
 @export var selected_weapon: String
 var reloading: bool = false
 var bullet_count: int = 0
-
 var player_health: int = 8
-
-var magazine_size: int = 20
-var bullet_damage: int = 60
-var reload_time: float = 1.2
-var shooting_speed: float = 0.3
-var spread: float = 0
-var bullets_per_shot = 1
+var magazine_size: int
+var bullet_damage: int
+var reload_time: float
+var shooting_speed: float
+var spread: float
+var bullets_per_shot: int
 
 
 func _ready() -> void:
