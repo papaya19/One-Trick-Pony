b25fc66f1d861756b4b3310dc200f8e52b606d9c
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-03-23 13:33:05 +1300

    music working
    yipee i pulled my weight and did something today

--- Godot Project Files/Scripts/music_player.gd
+++ Godot Project Files/Scripts/music_player.gd
@@ -3,25 +3,16 @@ extends AudioStreamPlayer
 var playlist: Array = [
 	preload("res://Assets/Sounds/Music/2026-03-17T03_57_05.313Z.wav"),
 	preload("res://Assets/Sounds/Music/sagar sandhra - New Project 2026-02-05 16_27.mp3"),
-	preload("res://Assets/Sounds/Music/sagar sandhra - New Project 2026-02-05 16_27.mp3"),
+	preload("res://Assets/Sounds/Music/sagar sandhra - New Project 2026-02-09 21_01.mp3"),
 	preload("res://Assets/Sounds/Music/thestrudelsong.wav")
 ]
 func _ready():
-	# Connect the signal that triggers when a song finishes
-	$".".finished.connect(_on_music_finished)
-	play_next_song()
+	play_random_song()
 
-func play_next_song():
-	# If the queue is empty, refill it and shuffle
-	if playlist.is_empty():
-		playlist = playlist.duplicate()
-		playlist.shuffle()
-	
-	# Pull the next song from the queue
-	var next_track = playlist.pop_back()
-	
-	$".".stream = next_track
-	$".".play()
+func play_random_song():
+	stream = playlist.pick_random()
+	play()
 
-func _on_music_finished():
-	play_next_song()
+# Optional: To play a new one automatically when the current song ends
+func _on_finished():
+	play_random_song()
